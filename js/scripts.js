bd30421ec66f4780d7edbd571aafb2a581d025ee
chloebarnarddesigns <79425619+chloebarnarddesigns@users.noreply.github.com>
2021-11-18 05:41:21 -0500

    atom beautify and error tests

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+console.log('Hello World')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  var isMobileDevice = window.innerWidth <= 640
+
+  if (x.className === 'navtoggle' && isMobileDevice) {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
